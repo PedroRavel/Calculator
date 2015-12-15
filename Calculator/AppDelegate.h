c0ed7cb81ba0b3dab8e0c926a55fa2f687530757
Pete <Pete@Petes-Mac.local>
2015-12-14 21:09:32 -0800

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Pete on 12/14/15.
+//  Copyright (c) 2015 Pete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Pete on 12/14/15.
+//  Copyright (c) 2015 Pete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
